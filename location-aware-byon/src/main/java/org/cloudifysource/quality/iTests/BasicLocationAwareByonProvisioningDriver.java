1a28a405fbb5cffc4302d6094df12d14412078ef
iliapolo <elip@gigaspaces.com>
2013-10-15 15:37:40 +0300

    fix compilation

--- location-aware-byon/src/main/java/org/cloudifysource/quality/iTests/BasicLocationAwareByonProvisioningDriver.java
+++ location-aware-byon/src/main/java/org/cloudifysource/quality/iTests/BasicLocationAwareByonProvisioningDriver.java
@@ -1,21 +1,39 @@
+/*
+ * Copyright (c) 2011 GigaSpaces Technologies Ltd. All rights reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.cloudifysource.quality.iTests;
 
 import org.cloudifysource.esc.driver.provisioning.CloudProvisioningException;
 import org.cloudifysource.esc.driver.provisioning.MachineDetails;
+import org.cloudifysource.esc.driver.provisioning.ProvisioningContext;
 import org.cloudifysource.esc.driver.provisioning.byon.ByonProvisioningDriver;
 
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+/**
+ *
+ */
 public class BasicLocationAwareByonProvisioningDriver extends ByonProvisioningDriver {
 
-    public MachineDetails startMachine(String locationId, long timeout, TimeUnit timeUnit)
-            throws TimeoutException, CloudProvisioningException
-    {
-        MachineDetails machineDetails = super.startMachine(locationId, timeout, timeUnit);
-        machineDetails.setLocationId(locationId);
+    @Override
+    public MachineDetails startMachine(final ProvisioningContext context, final long timeout, final TimeUnit timeUnit)
+            throws TimeoutException, CloudProvisioningException {
+        MachineDetails machineDetails = super.startMachine(context, timeout, timeUnit);
+        machineDetails.setLocationId(context.getLocationId());
         return machineDetails;
-
     }
-
 }
--- location-aware-byon/src/main/java/org/cloudifysource/quality/iTests/LocationAwareByonProvisioningDriver.java
+++ location-aware-byon/src/main/java/org/cloudifysource/quality/iTests/LocationAwareByonProvisioningDriver.java
@@ -2,7 +2,9 @@ package org.cloudifysource.quality.iTests;
 
 import org.cloudifysource.esc.driver.provisioning.CloudProvisioningException;
 import org.cloudifysource.esc.driver.provisioning.MachineDetails;
+import org.cloudifysource.esc.driver.provisioning.ProvisioningContext;
 import org.cloudifysource.esc.driver.provisioning.byon.ByonProvisioningDriver;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -14,10 +16,8 @@ public class LocationAwareByonProvisioningDriver extends ByonProvisioningDriver 
     private List<String> locationsToStartMachinesWith;
 	private AtomicInteger index;
 
-
-	
 	public LocationAwareByonProvisioningDriver () {
-		this.locationsToStartMachinesWith = Arrays.asList(getAvailabilityZones());
+        this.locationsToStartMachinesWith = Arrays.asList(getAvailabilityZones());
 		this.index = new AtomicInteger(0);
 	}
 
@@ -26,17 +26,18 @@ public class LocationAwareByonProvisioningDriver extends ByonProvisioningDriver 
         return new String[] { "zone1", "zone2", "zone3","zone4" };
     }
 
-    public MachineDetails startMachine(String locationId, long timeout, TimeUnit timeUnit)
+    @Override
+    public MachineDetails startMachine(ProvisioningContext context, long timeout, TimeUnit timeUnit)
             throws TimeoutException, CloudProvisioningException
     {
         MachineDetails machineDetails = null;
-        if (this.locationsToStartMachinesWith.contains(locationId)){
-            machineDetails = super.startMachine(locationId, timeout, timeUnit);
-            machineDetails.setLocationId(locationId);
+        if (this.locationsToStartMachinesWith.contains(context.getLocationId())){
+            machineDetails = super.startMachine(context, timeout, timeUnit);
+            machineDetails.setLocationId(context.getLocationId());
         }
         else {
             String location = this.locationsToStartMachinesWith.get(this.index.getAndIncrement() % this.locationsToStartMachinesWith.size());
-            machineDetails = super.startMachine(location, timeout, timeUnit);
+            machineDetails = super.startMachine(context, timeout, timeUnit);
             machineDetails.setLocationId(location);
 
         }
